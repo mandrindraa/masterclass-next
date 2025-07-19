8f62b72a0607bc802fa5144d7ececce1f38237a8
mandrindraa <mandrindraantonnio@gmail.com>
2025-07-19 12:09:57 +0300

    i18n

--- i18n.ts
+++ i18n.ts
@@ -3,7 +3,7 @@ import { getRequestConfig } from "next-intl/server";
 // Can be imported from a shared config
 const locales = ["en", "fr"];
 
-export default getRequestConfig(async ({ locale, requestLocale }) => {
+export default getRequestConfig(async ({ locale }) => {
   // Validate that the incoming `locale` parameter is valid
   if (!locales.includes(locale as any)) {
     // Use default locale instead of notFound()
